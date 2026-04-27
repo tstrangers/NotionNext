9bf946b023da0fc299dee35aadbf1643faf45c1e
tstrangers <1049987821@qq.com>
2026-04-27 22:27:39 +0800

    Update PoweredBy.js

--- components/PoweredBy.js
+++ components/PoweredBy.js
@@ -7,7 +7,13 @@ import { siteConfig } from '@/lib/config'
 export default function PoweredBy(props) {
   return (
     <div className={`inline text-sm font-serif ${props.className || ''}`}>
-      王帅 All Rights Reserved.
+      <span className='mr-1'>Powered by</span>
+      <a
+        href='https://github.com/tangly1024/NotionNext'
+        className='underline justify-start'>
+        NotionNext {siteConfig('VERSION')}
+      </a>
+      .
     </div>
   )
 }
