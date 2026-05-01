79bd5f16f679cf4c8afc3a9fce3100ff123ea3e1
tstrangers <1049987821@qq.com>
2026-05-01 21:14:19 +0800

    Update LoadingCover.js

--- themes/proxio/components/LoadingCover.js
+++ themes/proxio/components/LoadingCover.js
@@ -3,7 +3,7 @@ import { useEffect, useState } from 'react';
 
 const LoadingCover = ({ onFinishLoading }) => {
     const [isVisible, setIsVisible] = useState(true);
-    const welcomeText = siteConfig('PROXIO_WELCOME_TEXT', '欢迎来到我们的网站！');
+    const welcomeText = siteConfig('PROXIO_WELCOME_TEXT', '欢迎来到我的网站！');
 
     // 定义颜色变量
     const colors = {
